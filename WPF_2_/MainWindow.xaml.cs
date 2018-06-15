f724950e4a5f4566b8bc898a5477bf2f55b9adef
Vitaliy Alexandrovich <Dobrota2017@outlook.com>
2018-06-15 22:08:54 +0300

    Initial commit

---
+++ WPF_2_/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_2_
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Title = "Reminder";
+            for (int i = 0; i < 100; ++i)
+            {
+                
+                Button d = new Button();
+                d.Width = 50;
+                d.Height = 50;
+                d.Background = Brushes.Pink;
+                ListBoxC1.Items.Add(d);
+            }
+        }
+
+        private void TextBox_Scroll(object sender, System.Windows.Controls.Primitives.ScrollEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Grid_MouseMove(object sender, MouseEventArgs e)
+        {
+            
+        }
+    }
+}
